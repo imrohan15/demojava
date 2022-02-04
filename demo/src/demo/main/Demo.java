2be030755f233894f16435619eb2bb6805259dc0
imrohan15 <sehgalrohan619@gmail.com>
2022-02-04 09:38:24 +0530

    demojava commit

---
+++ demo/src/demo/main/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo.main;
+
+/**
+ * @author HP
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ demo/src/demo/main/MyClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo.main;
+
+/**
+ * @author HP
+ *
+ */
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my Class");
+
+	}
+
+}
